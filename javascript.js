851b60ff8eb74f381734584cfc30ad23dd6e89ea
Jake <jakevisc@gmail.com>
2022-05-11 11:14:24 -0230

    Create function to loop and display library array
    Created the displayCards(array) function which takes in the myLibrary
    array and lists each book as its own separate card on the page. I have
    also updated the addToLibrary(book) function to be correct and work
    properly.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,45 @@
-let myLibrary = []
+const HP = new Book('Harry Potter: Prisoner of Azkaban', 'J.K. Rowling', 317, true)
+const LOTR = new Book('Lord of the Rings', 'J.R.R. Tolkien', 347, false)
 
-function Book(title, author, numPages, isRead = false) {
+let myLibrary = [HP, LOTR]
+
+function Book(title, author, numPages, isRead) {
   this.title = title
   this.author = author
   this.numPages = numPages
   this.isRead = isRead
 }
 
-function addTolibrary() { 
-  myLibrary.push(new Book())
+function addTolibrary(book) { 
+  myLibrary.push(book)
 }
+
+function displayCards(array) {
+  let content = document.querySelector('.content')
+  array.forEach(book => {
+    let card = document.createElement('div')
+    card.classList.add('card')
+    let cardContent = ''
+    if (book.isRead) {
+      cardContent = `
+      <div class='title'>${book.title}</div>
+      <div class='author'>${book.author}</div>
+      <div class='numPages'>${book.numPages}</div>
+      <button class='isRead'>Already read</button>
+      <button class='remove'>Remove</button>
+      `
+    } else {
+      cardContent = `
+      <div class='title'>${book.title}</div>
+      <div class='author'>${book.author}</div>
+      <div class='numPages'>${book.numPages}</div>
+      <button class='isRead'>Not yet read</button>
+      <button class='remove'>Remove</button>
+      `
+    }
+    card.innerHTML = cardContent
+    content.appendChild(card)
+  })
+}
+
+displayCards(myLibrary)
